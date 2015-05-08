321e6eef0904e24e6827eee45d041d0b54dd2f7a
Daniel Ryding <daniel.ryding@infotrack.com.au>
2015-05-08 15:30:11 +1000

    Added async completion logic.

--- start.js
+++ start.js
@@ -1,46 +1,75 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 var request = require('request');
+var colors = require('colors');
 
 var url_file = 'urls.json';
-
 var urls = JSON.parse(fs.readFileSync(url_file, 'utf8'));
 
 var results = [];
 
-for (index = 0; index < urls.length; index++)
-{
-	results.push((function(url, index) {
-		request(url, function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    var $ = cheerio.load(body);
-
-		    var streetAddress = $('span[itemprop="streetAddress"]').html();
-		    var addressLocality = $('span[itemprop="addressLocality"]').html();
-		    var priceText = $('.priceText').html();
-
-		    console.log('==================================================================================================');
-		    console.log((index + 2) + ': ' + url);
-		    console.log('==================================================================================================');
-		    console.log(streetAddress);
-		    console.log(addressLocality);
-			console.log(priceText);
-
-			// Get Inspection Events
-		    $('a[itemprop="events"]').each(function (index) {
-		    	var name = $(this).find('strong[itemprop="name"]').text();
-		    	var time = $(this).find('.time').text();
-		    	console.log(name + ": " + time);
-		    });
-		  }
-		  return streetAddress;
-		})
-	})(urls[index], index));
+var counter = urls.length;
+var callback = function (index, accumulator) {
+	results[index] = accumulator;
+	counter --;
+	if (counter == 0) {
+		// Finished
+		PrintResults(results);
+	}
 }
-console.log('================');
-console.log(results);
 
-function hello()
+for (index = urls.length - 1; index >= 0; index--)
 {
-	console.log('Hello!');
+	uRLFetcher(urls, index, callback);
+}
+
+function uRLFetcher(urls, index, callback) 
+{
+	request(urls[index], function handleResponse(error, response, body) {
+		if (!error && response.statusCode == 200) 
+		{
+			var result = extractDetails(urls[index], body, index);
+		}
+		else
+		{
+			var result = null;
+		}
+		callback(index, result);
+	})
+}
+
+function extractDetails(url, htmlbody, index)
+{
+	var $ = cheerio.load(htmlbody);
+
+	var streetAddress = $('span[itemprop="streetAddress"]').html();
+	var addressLocality = $('span[itemprop="addressLocality"]').html();
+	var priceText = $('.priceText').html();
+
+	// Get Inspection Events
+	var inspectionTimes = [];
+	$('a[itemprop="events"]').each(function () {
+		var name = $(this).find('strong[itemprop="name"]').text();
+		var time = $(this).find('.time').text();
+		inspectionTimes.push(name + ": " + time);
+	});
+
+	return { "index": index + 1, "uRL": url, "address": streetAddress + ', ' + addressLocality, "price": priceText, "inspectionTimes": inspectionTimes };
+}
+
+function PrintResults(results)
+{
+	for (i = 0; i < results.length; i++)
+	{
+		if (results[i] !== null)
+		{
+			console.log('=================================================================================================='.grey);
+			console.log((results[i].index + ': ' + results[i].uRL).yellow);
+			console.log(results[i].address);
+			console.log(results[i].price);
+			for (j = 0; j < results[i].inspectionTimes.length; j++) {
+				console.log(results[i].inspectionTimes[j].red);
+			}
+		}
+	}
 }
